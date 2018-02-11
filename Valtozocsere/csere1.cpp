1a47a0905ebb947e17e26f17cfc451c8a0596acb
markkatona <36377673+markkatona@users.noreply.github.com>
2018-02-11 18:48:10 +0100

    Add files via upload

---
+++ Valtozocsere/csere1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int a= 2018;
+int b= 42;
+cout << "a = " << a << ", b = " << b << endl;
+
+//csere
+a = a + b;
+b = a - b;
+a = a - b;
+cout << "a = " << a << ", b = " << b << endl;
+
+//visszacsere	
+a = a + b;
+b = a - b;
+a = a - b;
+cout << "a = " << a << ", b = " << b << endl;
+return 0; 
+}
+
